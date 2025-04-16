f60299e56665dc69527ad352e17494764a0ac0b6
Philippe Marchand <PHMAR122@ulaval.ca>
2025-04-16 16:53:33 -0400

    Save5

--- problimite.py
+++ problimite.py
@@ -2,4 +2,10 @@ import numpy as np
 from tridiagonal import tridiagonal
 
 def problimite(h, P, Q, R, a, b, alpha, beta):
+    N = len(P) - 2
+    D = np.zeros(N)
+    I = np.zeros(N - 1)
+    S = np.zeros(N - 1)
+    b_vec = np.zeros(N)
+
     
