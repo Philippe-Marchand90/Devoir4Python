4c06338c08a524b8244a0f8d2725add88df6b9af
Philippe Marchand <PHMAR122@ulaval.ca>
2025-04-16 16:45:14 -0400

    Save2

--- tridiagonal.py
+++ tridiagonal.py
@@ -5,3 +5,7 @@ from scipy.sparse.linalg import spsolve
 
 
 def solve_tridiagonal(I, D, S, b):
+    A = diags([I, D, S], offsets=[-1, 0, 1])
+    A_csr = csr_matrix(A)
+    x = spsolve(A_csr, b)
+    return x
